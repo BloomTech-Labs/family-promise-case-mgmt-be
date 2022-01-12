ed7d9e47134b21a5bcc6b4724f38b294849efbb2
miketillman95 <matillman95@gmail.com>
2022-01-11 20:55:37 -0700

    fixed names

--- data/migrations/20220111191506_family_form.js
+++ data/migrations/20220111191506_family_form.js
@@ -3,13 +3,13 @@ exports.up = function(knex) {
     return knex
     .createTable('family_form', function(tbl) {
         tbl.increments()
-        tbl.string('Head of Household', 128).notNullable()
-        tbl.string('Family members', 128).notNullable()
-        tbl.integer('Family size').notNullable()
-        tbl.string('Education level', 128).notNullable()
-        tbl.integer('Social security number', 9).notNullable()
-        tbl.string('Prior history of homlessness', 300)
-        tbl.boolean('Employment')
+        tbl.string('head_of_household', 128).notNullable()
+        tbl.string('family_members', 128).notNullable()
+        tbl.integer('family_size').notNullable()
+        tbl.string('education_level', 128).notNullable()
+        tbl.integer('social_security_number', 9).notNullable()
+        tbl.string('homeless_history', 300)
+        tbl.boolean('employment')
 
     })
 
