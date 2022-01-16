22d0658f68e8ff22b4ad93506da677a64a8fd154
miketillman95 <matillman95@gmail.com>
2022-01-16 15:22:16 -0700

    client crud enpoints complete

--- api/clients/clientrouter.js
+++ api/clients/clientrouter.js
@@ -2,7 +2,73 @@ const router = require('express').Router()
 const Clients = require('./clientModel')
 
 
-router.get
+router.get('/', (req, res, next) => {
+    Clients.find(req.query)
+    .then(clients => {
+        res.status(200).json(clients)
+    })
+    // sending these to the route handlers as well
+    .catch(next)
+})
 
 
 
+router.get('/:id', (req, res, next) => {
+    Clients.findById(req.params.id)
+    .then(clients => {
+        if(clients) {
+            res.status.json({clients})
+        } else{
+            res.status(404).json({
+                message: 'Client could not be found'
+            })
+        }
+
+    })
+    .catch(next)
+})
+
+router.post('/', (req, res, next) => {
+    Clients.add(req.body)
+    .then(clients => {
+        res.status(201).json(clients)
+    })
+    .catch(next)
+})
+
+
+router.delete(':id', (req, res, next) => {
+    Clients.remove(req.params.id)
+    .then(count => {
+        if(count >0) {
+            res.status(200).json({
+                message: 'Client has been removed'
+            })
+        } else{
+            res.status(404).json({
+                message: 'Client could not be found'
+            })
+        }
+    })
+    .catch(next)
+})
+
+router.put(':id', (req, res, next) => {
+    const changes = req.body
+    Clients.updates(req.params.id, changes)
+    .then(clients => {
+        if(clients) {
+            res.status(200).json({
+                message: 'Clients has been updated'
+            })
+        }else{
+            // i see 404 error handlers  in app.js, could update these to go to them instead 
+            res.status(404).json({
+                message: 'Clients could not be found'
+            })
+        }
+    })
+    .catch(next)
+})
+
+module.exports = router
--- data/migrations/20220111191506_client_form.js
+++ data/migrations/20220111191506_client_form.js
@@ -1,17 +1,17 @@
 
 exports.up = function(knex) {
-    return knex
+    return knex.schema
     .createTable('clients', function(tbl) {
-        tbl.increments().references('client_id').inTable('education_histories')
-        tbl.int('household_id').notNullable().references('id').inTable('households')
+        tbl.increments().references('client_id').inTable('education_histories').onDelete('RESTRICT')
+        tbl.int('household_id').notNullable().references('id').inTable('households').onDelete('RESTRICT')
         tbl.string('first_name',).notNullable()
         tbl.string('last_name').notNullable()
         tbl.int('ssn', 9).notNullable()
         tbl.boolean('is_hoh').notNullable()
         tbl.string('relation').notNullable()
-       tbl.int('gender_id').references('id').inTable('genders')
-        tbl.int('race_id').references('id').inTable('races')
-        tbl.int('ethnicity_id').references('id').inTable('ethnicities')
+       tbl.int('gender_id').references('id').inTable('genders').onDelete('RESTRICT')
+        tbl.int('race_id').references('id').inTable('races').onDelete('RESTRICT')
+        tbl.int('ethnicity_id').references('id').inTable('ethnicities').onDelete('RESTRICT')
         // says date time in the design but timestamp might work better for a created_at/deleted_at?
         tbl.timestamp('created_at', {useTz: true})
     })  
