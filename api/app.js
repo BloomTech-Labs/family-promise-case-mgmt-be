5faed3a6db557f0ae78e27ecaa6cf61fb30ce7b3
miketillman95 <matillman95@gmail.com>
2022-01-15 20:29:44 -0700

    modle complete

--- api/app.js
+++ api/app.js
@@ -22,6 +22,7 @@ const swaggerUIOptions = {
 const indexRouter = require('./index/indexRouter');
 const profileRouter = require('./profile/profileRouter');
 const dsRouter = require('./dsService/dsRouter');
+const clientsRouter= require('./clients/clientrouter')
 
 const app = express();
 
@@ -51,6 +52,7 @@ app.use(cookieParser());
 app.use('/', indexRouter);
 app.use(['/profile', '/profiles'], profileRouter);
 app.use('/data', dsRouter);
+app.use('/clients', clientsRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ api/clients/clientrouter.js
@@ -0,0 +1,8 @@
+const router = require('express').Router()
+const Clients = require('./clientModel')
+
+
+router.get
+
+
+
