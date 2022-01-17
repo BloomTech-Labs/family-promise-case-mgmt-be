6cdaec81c7ac1eb687c2f7ea612d38cc99eae453
slothwithannuzzi <78941408+slothwithannuzzi@users.noreply.github.com>
2022-01-17 13:38:21 -0600

    created phone_numbers, email_addresses, and education_histories tables. Also updated profiles table to reflect users table in schema

--- data/migrations/20200625220949_create-profile.js
+++ data/migrations/20200625220949_create-profile.js
@@ -1,15 +1,13 @@
 exports.up = (knex) => {
-  return knex.schema
-    .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
-    .createTable('profiles', function (table) {
-      table.string('id').notNullable().unique().primary();
-      table.string('email');
-      table.string('name');
-      table.string('avatarUrl');
-      table.timestamps(true, true);
-    });
+  return knex.schema.createTable('users', function (table) {
+    table.increments('id').notNullable().unique().primary();
+    table.string('email').notNullable();
+    table.string('name').notNullable();
+    table.string('role').notNullable();
+    table.timestamps('created_at').defaultTo(knex.fn.now());
+  });
 };
 
 exports.down = (knex) => {
-  return knex.schema.dropTableIfExists('profiles');
+  return knex.schema.dropTableIfExists('users');
 };
--- data/migrations/20220117123847_case_mgmt_schema.js
+++ data/migrations/20220117123847_case_mgmt_schema.js
@@ -1,58 +1,85 @@
 exports.up = function (knex) {
   return knex.schema
-  .createTable('locations', (t) => {
-    t.increments('ID').notNullable().unique().primary();
-    t.integer('household_id').notNullable();
-    t.string('type');
-    t.string('name');
-    t.string('latlong');
-    t.string('address1');
-    t.string('address2');
-    t.string('city');
-    t.string('state');
-    t.string('zip');
-    t.timestamp('deleted_at');
-    t.timestamp('created_at');
-  })
-  .createTable('households', (t) => {
-    t.increments('ID').notNullable().unique().primary();
-    t.string('name').notNullable();
-    t.timestamp('created_at');
-  })
-  .createTable('clients', (t) => {
-    t.increments('ID').notNullable().unique().primary();
-    t.integer('household_id').notNullable();
-    t.string('first_name').notNullable();
-    t.string('last_name').notNullable();
-    t.string('ssn').notNullable();
-    t.boolean('is_hoh');
-    t.string('relation');
-    t.string('education_level');
-    t.integer('gender_id');
-    t.integer('race_id');
-    t.integer('ethnicity_id');
-    t.timestamp('created_at');
-  })
-  .createTable('genders', (t) => {
+    .createTable('locations', (t) => {
+      t.increments('ID').notNullable().unique().primary();
+      t.integer('household_id').notNullable();
+      t.string('type');
+      t.string('name');
+      t.string('latlong');
+      t.string('address1');
+      t.string('address2');
+      t.string('city');
+      t.string('state');
+      t.string('zip');
+      t.timestamp('deleted_at').defaultTo(knex.fn.now());
+      t.timestamp('created_at').defaultTo(knex.fn.now());
+    })
+    .createTable('households', (t) => {
+      t.increments('ID').notNullable().unique().primary();
+      t.string('name').notNullable();
+      t.timestamp('created_at').defaultTo(knex.fn.now());
+    })
+    .createTable('clients', (t) => {
+      t.increments('ID').notNullable().unique().primary();
+      t.integer('household_id').notNullable();
+      t.string('first_name').notNullable();
+      t.string('last_name').notNullable();
+      t.string('ssn').notNullable();
+      t.boolean('is_hoh');
+      t.string('relation');
+      t.string('education_level');
+      t.integer('gender_id');
+      t.integer('race_id');
+      t.integer('ethnicity_id');
+      t.timestamp('created_at').defaultTo(knex.fn.now());
+    })
+    .createTable('genders', (t) => {
       t.increments('ID').notNullable().unique().primary();
       t.string('name').notNullable().unique();
-  })
-  .createTable('races', (t) => {
+    })
+    .createTable('races', (t) => {
       t.increments('ID').notNullable().unique().primary();
       t.string('name').notNullable().unique();
-  })
-  .createTable('ethnicities', (t) =>{
-    t.increments('ID').notNullable().unique().primary();
-    t.string('name').notNullable().unique();
-  })
+    })
+    .createTable('ethnicities', (t) => {
+      t.increments('ID').notNullable().unique().primary();
+      t.string('name').notNullable().unique();
+    })
+    .createTable('phone_numbers', (t) => {
+      t.increments('ID').notNullable().unique().primary();
+      t.integer('client_id').notNullable();
+      t.string('number').notNullable().unique();
+      t.string('phone_type');
+      t.timestamp('deleted_at').defaultTo(knex.fn.now());
+      t.timestamp('created_at').defaultTo(knex.fn.now());
+    })
+    .createTable('email_addresses', (t) => {
+      t.increments('ID').notNullable().unique().primary();
+      t.integer('client_id').notNullable();
+      t.string('email').notNullable().unique();
+      t.string('email_type');
+      t.booelan('allow_sms');
+      t.timestamp('deleted_at').defaultTo(knex.fn.now());
+      t.timestamp('created_at').defaultTo(knex.fn.now());
+    })
+    .createTable('education_histories', (t) => {
+      t.increments('ID').notNullable().unique().primary();
+      t.integer('client_id').notNullable();
+      t.string('school_name');
+      t.string('level');
+      t.date('start_date');
+      t.date('end_date');
+    });
 };
 
 exports.down = function (knex) {
   return knex.schema
+    .dropTableIfExists('email_addresses')
+    .dropTableIfExists('phone_numbers')
     .dropTableIfExists('ethnicities')
     .dropTableIfExists('races')
     .dropTableIfExists('genders')
     .dropTableIfExists('clients')
     .dropTableIfExists('households')
-    .dropTableIfExists('locations')
+    .dropTableIfExists('locations');
 };
