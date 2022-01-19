36395fa2093d969114c4a4d10dbb04458aad804b
miketillman95 <matillman95@gmail.com>
2022-01-18 17:44:35 -0700

    tables

--- data/migrations/20220111191506_client_form.js
+++ data/migrations/20220111191506_client_form.js
@@ -3,21 +3,23 @@ exports.up = function(knex) {
     return knex.schema
     .createTable('clients', function(tbl) {
         
-        tbl.increments().references('client_id').inTable( 'education_histories').onDelete('RESTRICT')
+        tbl.increments().references('client_id').inTable( 'client_notes').onDelete('RESTRICT').primary()
+        tbl.foreign('clients_id')
         tbl.integer('household_id').notNullable().references('id').inTable('households').onDelete('RESTRICT')
         tbl.string('first_name',).notNullable()
         tbl.string('last_name').notNullable()
         tbl.varchar('ssn', 9).notNullable()
         tbl.boolean('is_hoh').notNullable()
         tbl.string('relation').notNullable()
-       tbl.integer('gender_id').references('id').inTable('genders').onDelete('RESTRICT')
-        tbl.integer('race_id').references('id').inTable('races').onDelete('RESTRICT')
-        tbl.integer('ethnicity_id').references('id').inTable('ethnicities').onDelete('RESTRICT')
+        tbl.uuid('gender_id').references('id').inTable('genders').onDelete('RESTRICT')
+        tbl.uuid('race_id').references('id').inTable('races').onDelete('RESTRICT')
+        tbl.uuid('ethnicity_id').references('id').inTable('ethnicities').onDelete('RESTRICT')
         tbl.timestamp('created_at', {useTz: true})
     })
     
     .createTable('genders', tbl => {
         tbl.increments().references('gender_id').inTable('clients')
+        tbl.foreign('gender_id')
         tbl.string('name')
     })
 
