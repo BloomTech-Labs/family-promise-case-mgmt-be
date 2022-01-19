55010c33f8a8b0edcb323a26a96f32b83aaf40fb
slothwithannuzzi <78941408+slothwithannuzzi@users.noreply.github.com>
2022-01-18 19:42:01 -0600

    fixed user table migration file to be prettier

--- data/migrations/20220118193003_user-table.js
+++ data/migrations/20220118193003_user-table.js
@@ -1,16 +1,13 @@
-
-exports.up = function(knex) {
-  return knex.schema
-    .createTable('users', (t) => {
-        t.increments('ID').notNullable().unique().primary();
-        t.string('name').notNullable().unique();
-        t.string('email').notNullable().unique();
-        t.string('role').notNullable();
-        t.timestamp('created_at').defaultTo(knex.fn.now());
-    })
+exports.up = function (knex) {
+  return knex.schema.createTable('users', (t) => {
+    t.increments('ID').notNullable().unique().primary();
+    t.string('name').notNullable().unique();
+    t.string('email').notNullable().unique();
+    t.string('role').notNullable();
+    t.timestamp('created_at').defaultTo(knex.fn.now());
+  });
 };
 
-exports.down = function(knex) {
-  return knex.schema
-    .dropTableIfExists('users');
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('users');
 };
