40552e5c59356f2ef06cbbd03603de5835b1702b
ashtilawat23 <ashtilawat23@gmail.com>
2022-01-24 23:10:24 -0500

    Fixed linting errors in failing tests

--- api/client/clientModel.js
+++ api/client/clientModel.js
@@ -1,41 +1,33 @@
-const db = require('../../data/db-config')
-
+const db = require('../../data/db-config');
 
 // meed help mocking this data for future reference
 
-const add = async(newClient) => {
-    const [id] = await db('clients')
-    .insert(newClient)
+const add = async (newClient) => {
+  const [id] = await db('clients').insert(newClient);
 
-    return findById(id)
-}
+  return findById(id);
+};
 
 const findAll = () => {
-    return db('clients')
-}
+  return db('clients');
+};
 
 const findById = async (id) => {
-    return db('clients')
-    .where({id})
-    .first()
-}
+  return db('clients').where({ id }).first();
+};
 
 const remove = (id) => {
-    return db('clients')
-    .where({id})
-    .del()
-}
+  return db('clients').where({ id }).del();
+};
 
 const update = (id, changes) => {
-    return db('clients')
-    .where({id})
-    .update(changes, '*')
-}
+  return db('clients').where({ id }).update(changes, '*');
+};
 
 module.exports = {
-    add,
-    findById,
-    findAll,
-    update, 
-    remove
-}
+  add,
+  findById,
+  findAll,
+  update,
+  remove,
+};
