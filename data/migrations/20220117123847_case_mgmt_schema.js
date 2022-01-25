f710366e89041db7d6686cc5a4cc81d15da7e994
slothwithannuzzi <78941408+slothwithannuzzi@users.noreply.github.com>
2022-01-24 19:06:58 -0600

    Features/migrations (#1)
    * created knex file
    
    * tailored knex file to project specs
    
    * created new migration file and locations table
    
    * created households table
    
    * created clients table
    
    * created genders, races, and ethnicities tables
    
    * created phone_numbers, email_addresses, and education_histories tables. Also updated profiles table to reflect users table in schema
    
    * added a forgotten dropTable command for 'education_histories'
    
    * removed migration table that was causing error
    
    * removed useless auto generated seed file
    
    * changed PORT back to 5400, changed timestamps into strings
    
    * removed accidental changes made to package.json

---
+++ data/migrations/20220117123847_case_mgmt_schema.js
@@ -0,0 +1,86 @@
+exports.up = function (knex) {
+  return knex.schema
+    .createTable('locations', (t) => {
+      t.increments('ID').notNullable().unique().primary();
+      t.integer('household_id').notNullable();
+      t.string('type');
+      t.string('name');
+      t.string('latlong');
+      t.string('address1');
+      t.string('address2');
+      t.string('city');
+      t.string('state');
+      t.string('zip');
+      t.string('deleted_at').defaultTo(knex.fn.now());
+      t.string('created_at').defaultTo(knex.fn.now());
+    })
+    .createTable('households', (t) => {
+      t.increments('ID').notNullable().unique().primary();
+      t.string('name').notNullable();
+      t.string('created_at').defaultTo(knex.fn.now());
+    })
+    .createTable('clients', (t) => {
+      t.increments('ID').notNullable().unique().primary();
+      t.integer('household_id').notNullable();
+      t.string('first_name').notNullable();
+      t.string('last_name').notNullable();
+      t.string('ssn').notNullable();
+      t.boolean('is_hoh');
+      t.string('relation');
+      t.string('education_level');
+      t.integer('gender_id');
+      t.integer('race_id');
+      t.integer('ethnicity_id');
+      t.string('created_at').defaultTo(knex.fn.now());
+    })
+    .createTable('genders', (t) => {
+      t.increments('ID').notNullable().unique().primary();
+      t.string('name').notNullable().unique();
+    })
+    .createTable('races', (t) => {
+      t.increments('ID').notNullable().unique().primary();
+      t.string('name').notNullable().unique();
+    })
+    .createTable('ethnicities', (t) => {
+      t.increments('ID').notNullable().unique().primary();
+      t.string('name').notNullable().unique();
+    })
+    .createTable('phone_numbers', (t) => {
+      t.increments('ID').notNullable().unique().primary();
+      t.integer('client_id').notNullable();
+      t.string('number').notNullable().unique();
+      t.string('phone_type');
+      t.string('deleted_at').defaultTo(knex.fn.now());
+      t.string('created_at').defaultTo(knex.fn.now());
+    })
+    .createTable('email_addresses', (t) => {
+      t.increments('ID').notNullable().unique().primary();
+      t.integer('client_id').notNullable();
+      t.string('email').notNullable().unique();
+      t.string('email_type');
+      t.boolean('allow_sms');
+      t.string('deleted_at').defaultTo(knex.fn.now());
+      t.string('created_at').defaultTo(knex.fn.now());
+    })
+    .createTable('education_histories', (t) => {
+      t.increments('ID').notNullable().unique().primary();
+      t.integer('client_id').notNullable();
+      t.string('school_name');
+      t.string('level');
+      t.string('start_date');
+      t.string('end_date');
+    });
+};
+
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists('education_histories')
+    .dropTableIfExists('email_addresses')
+    .dropTableIfExists('phone_numbers')
+    .dropTableIfExists('ethnicities')
+    .dropTableIfExists('races')
+    .dropTableIfExists('genders')
+    .dropTableIfExists('clients')
+    .dropTableIfExists('households')
+    .dropTableIfExists('locations');
+};
---
+++ knexfile.js
@@ -0,0 +1,10 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: { directory: './data/migrations' },
+    seeds: { directory: './data/seeds' },
+  },
+};
