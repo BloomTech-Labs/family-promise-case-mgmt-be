24f85963e4d7767d3cff7e0f4b4e15111cf91f74
slothwithannuzzi <78941408+slothwithannuzzi@users.noreply.github.com>
2022-01-25 19:06:33 -0600

    copied over original migration code from original create-profile migration file

--- data/migrations/20220125190039_create-profile.js
+++ data/migrations/20220125190039_create-profile.js
@@ -1,8 +1,15 @@
-
-exports.up = function(knex) {
+exports.up = (knex) => {
+    return knex.schema
+      .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+      .createTable('profiles', function (table) {
+        table.string('id').notNullable().unique().primary();
+        table.string('email');
+        table.string('name');
+        table.string('avatarUrl');
+        table.timestamps(true, true);
+      });
+  };
   
-};
-
-exports.down = function(knex) {
-  
-};
+  exports.down = (knex) => {
+    return knex.schema.dropTableIfExists('profiles');
+  };
