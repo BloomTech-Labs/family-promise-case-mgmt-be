7dc629ef03e1118913dabbe813dcbaa052b00bbf
thecuriousones <tamaraleonard46@gmail.com>
2022-02-16 19:20:32 -0500

    notes model is completed for inital attempt

--- api/clientnotes/notesModel.js
+++ api/clientnotes/notesModel.js
@@ -4,6 +4,35 @@ function get() {
   return db('client_notes');
 }
 
+function getById(id) {
+  return db('client_notes').where({ id }).first();
+}
+
+function insert() {
+  return db('client_notes')
+    .insert('notes')
+    .then((id) => {
+      return getById(id[0]);
+    });
+}
+
+function update(id, changes) {
+  return db('client_notes')
+    .where(id)
+    .update(changes)
+    .then((id) => {
+      return getById(id);
+    });
+}
+
+function remove(id) {
+  return db('client_notes').where('id', id).del();
+}
+
 module.exports = {
   get,
+  getById,
+  insert,
+  update,
+  remove,
 };
