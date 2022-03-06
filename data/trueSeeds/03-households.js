ad6471173f32777f0991ca6881e980d08b4ef99c
thecuriousones <tamaraleonard46@gmail.com>
2022-03-06 14:58:03 -0500

    seed change

--- data/trueSeeds/03-households.js
+++ data/trueSeeds/03-households.js
@@ -5,7 +5,7 @@ exports.seed = function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex('households').insert([
-        { name: 'Johnson' }, //discuss with Jake about create_at
+        { name: 'Last NAme' }, //discuss with Jake about create_at
         { name: 'Goldberg' },
         { name: 'Smith' },
       ]);
