6553618410a94b8f6d4b9d09eaf9e08dc7222918
sayo mizoguchi <sl.deve080412@gmail.com>
2022-03-22 11:30:25 -0700

    updated households migrations

--- data/migrations/20220310092645_client_households_schema.js
+++ data/migrations/20220310092645_client_households_schema.js
@@ -1,36 +1,29 @@
 exports.up = function (knex) {
-  return knex.schema
-    .alterTable('households', (tbl) => {
-      tbl.dropColumn('created_at');
-    })
-    .alterTable('households', (tbl) => {
-      tbl.datetime('created_at').defaultTo(knex.fn.now());
-      tbl.integer('times_homeless_three_years');
-      tbl.integer('total_time_homeless_three_years_years');
-      tbl.integer('total_time_homeless_three_years_monthes');
-      tbl.integer('total_time_homeless_past_year_year');
-      tbl.integer('total_time_homeless_past_year_monthes');
-      tbl.boolean('previous_living_situation_non_human_habitation');
-      tbl.boolean('previous_living_situation_emergency_shelter');
-      tbl.boolean('previous_living_situation_unsheltered');
-      tbl.integer('previous_stay_length_years');
-      tbl.integer('previous_stay_length_monthes');
-      tbl.boolean('needs_interpreter');
-      tbl.boolean('access_to_private_transportation');
-      tbl.string('documented_disability_in_client_or_family');
-      tbl.boolean('needs_interpreter');
-      //I have to put disability section here!!
-      tbl.string('client_or_family_history_physical_illness');
-      tbl.string('client_or_family_history_mental_illness');
-      tbl.string('client_or_family_history_personal_violence');
-      tbl.string('client_or_family_history_substance_dependency');
-      tbl.boolean('cps_involvement');
-      tbl.boolean('cps_involvement_active');
-      tbl.string('dcyf_contact_name');
-      tbl.string('dcyf_contact_email');
-      tbl.string('dcyf_contact_phone_number');
-      tbl.boolean('section_8_voucher_lost');
-    });
+  return knex.schema.alterTable('households', (tbl) => {
+    tbl.integer('times_homeless_three_years');
+    tbl.integer('total_time_homeless_three_years_years');
+    tbl.integer('total_time_homeless_three_years_monthes');
+    tbl.integer('total_time_homeless_past_year_year');
+    tbl.integer('total_time_homeless_past_year_monthes');
+    tbl.boolean('previous_living_situation_non_human_habitation');
+    tbl.boolean('previous_living_situation_emergency_shelter');
+    tbl.boolean('previous_living_situation_unsheltered');
+    tbl.integer('previous_stay_length_years');
+    tbl.integer('previous_stay_length_monthes');
+    tbl.boolean('needs_interpreter');
+    tbl.boolean('access_to_private_transportation');
+    tbl.string('documented_disability_in_client_or_family');
+    tbl.string('client_or_family_history_physical_illness');
+    tbl.string('client_or_family_history_mental_illness');
+    tbl.string('client_or_family_history_personal_violence');
+    tbl.string('client_or_family_history_substance_dependency');
+    tbl.boolean('cps_involvement');
+    tbl.boolean('cps_involvement_active');
+    tbl.string('dcyf_contact_name');
+    tbl.string('dcyf_contact_email');
+    tbl.string('dcyf_contact_phone_number');
+    tbl.boolean('section_8_voucher_lost');
+  });
 };
 
 exports.down = function (knex) {
