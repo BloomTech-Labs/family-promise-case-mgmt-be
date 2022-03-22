6965394c429b53624f6b97050a85221f3e5442b0
sayo mizoguchi <sl.deve080412@gmail.com>
2022-03-22 12:10:24 -0700

    updated households seeds

--- data/migrations/20220310092645_client_households_schema.js
+++ data/migrations/20220310092645_client_households_schema.js
@@ -2,17 +2,17 @@ exports.up = function (knex) {
   return knex.schema.alterTable('households', (tbl) => {
     tbl.integer('times_homeless_three_years');
     tbl.integer('total_time_homeless_three_years_years');
-    tbl.integer('total_time_homeless_three_years_monthes');
-    tbl.integer('total_time_homeless_past_year_year');
-    tbl.integer('total_time_homeless_past_year_monthes');
+    tbl.integer('total_time_homeless_three_years_months');
+    tbl.integer('total_time_homeless_past_year_years');
+    tbl.integer('total_time_homeless_past_year_months');
     tbl.boolean('previous_living_situation_non_human_habitation');
     tbl.boolean('previous_living_situation_emergency_shelter');
     tbl.boolean('previous_living_situation_unsheltered');
     tbl.integer('previous_stay_length_years');
-    tbl.integer('previous_stay_length_monthes');
+    tbl.integer('previous_stay_length_months');
     tbl.boolean('needs_interpreter');
     tbl.boolean('access_to_private_transportation');
-    tbl.string('documented_disability_in_client_or_family');
+    // tbl.string('documented_disability_in_client_or_family');
     tbl.string('client_or_family_history_physical_illness');
     tbl.string('client_or_family_history_mental_illness');
     tbl.string('client_or_family_history_personal_violence');
