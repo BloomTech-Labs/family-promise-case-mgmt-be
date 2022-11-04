51140cd08ce542d344cf24b2ecf4942a315b2bd8
guest hugh <kbramall11@gmail.com>
2022-11-03 18:55:44 -0600

    fixed a table name in the contact seed

--- data/seeds/18-contact_preferences.js
+++ data/seeds/18-contact_preferences.js
@@ -1,10 +1,10 @@
 exports.seed = function (knex) {
   // Deletes ALL existing entries
-  return knex('table_name')
+  return knex('contact_preferences')
     .del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('contact_preferences').insert([
         {
           client_id: 1,
           food_assistance: true,
